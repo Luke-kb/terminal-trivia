8657beecc6719946720238fcaa0639253ce7ef40
Luke-kb <keananbrown@gmail.com>
2020-04-23 21:52:54 +1000

    linked gems and other ruby files

--- projects/terminal-app/src/app.rb
+++ projects/terminal-app/src/app.rb
@@ -1,56 +1,69 @@
-require 'httparty'
 require 'tty-prompt'
+require 'tty-cursor'
+require 'artii'
+require_relative './QuestionBank.rb'
+require_relative './methods.rb'
+require_relative '/Score.rb'
 
-class Question
-  attr_reader :category, :difficulty, :question, :correct_answer, :incorrect_answers
+welcome
+any_key
+check_username
 
-  def initialize
+# puts quiz.question_bank
+# puts quiz.q_prompt1
+# puts quiz.q_prompt2
+# puts quiz.q_prompt3
+# puts quiz.q_prompt4
+# puts quiz.q_prompt5
+# puts "---------------"
+# puts quiz.correct_answer1
+# puts quiz.correct_answer2
+# puts quiz.correct_answer3
+# puts quiz.correct_answer4
+# puts quiz.correct_answer5
 
-    @url = "https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple"
-    response = HTTParty.get(@url)
+# def ask_questions
+#   quiz = QuestionBank.new
 
-    #assign returned results to variable
-    question_bank = response.parsed_response
+#   loop do
+#     #create tty prompt instance
+#     prompt = TTY::Prompt.new
+#     #add in Q data and shuffle the order of the answer options each time
+#     user_answer = prompt.select("#{quiz.q_prompt1}", quiz.incorrect_answers1.push(quiz.correct_answer1).shuffle)
+    
+#     if user_answer == quiz.correct_answer1
+#       puts @score =+ 1
+#       prompt.keypress("Press any key to continue")
+#     else
+#       puts "damn"
+#       prompt.keypress("Press any key to continue")
+#     end
+#   end
+# end
 
-    # print all values for reference
-    # p question_bank
-
-    # assign variables to keys in results
-    @category = question_bank["results"][0]["category"]
-    # p @category
-
-    @difficulty = question_bank["results"][0]["difficulty"]
-    # p @difficulty
-
-    @question = question_bank["results"][0]["question"]
-    # p @question
-
-    @correct_answer = question_bank["results"][0]["correct_answer"]
-    # p @correct_answer
-
-    @incorrect_answers = question_bank["results"][0]["incorrect_answers"]
-    # p @incorrect_answers
-
-  end
-
-end
-
-question1 = Question.new
-# p question1.question
-
-
-prompt = TTY::Prompt.new
-#sets up Q and shuffles the order of the answer options
-user_answer1 = prompt.select("#{question1.question}", question1.incorrect_answers.push(question1.correct_answer).shuffle)
-
-if user_answer1 == question1.correct_answer
-  puts "Nice!"
-else
-  puts "incorrect"
-end
+# ask_questions
 
 
-# prompt.keypress("Press any key  to continue")
+#TODO
+# 1 need to find a way to keep score after each question (what kind of variable?)
+# 2 need to create a quiz class that runs the whole app
+# 3 fine-tune quiz loop 
+# 4 give user option to enter their name (tty-prompt)
+# 5 title graphic (ascii art? figlet/lolcat?)
+# 6 scoreboard at end of quiz (Score class)
 
+
+# quiz = QuestionBank.new
+
+# quiz.each_key do |key, value|
+#   puts "#{key} and the #{value}"
+# end
+
+
+# #create tty prompt instance
+# prompt = TTY::Prompt.new
+# #add in Q data and shuffle the order of the answer options each time
+# user_answer = prompt.select("#{quiz.q_prompt1}", quiz.incorrect_answers1.push(quiz.correct_answer1).shuffle)
+    
 
 
