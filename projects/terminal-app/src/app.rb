a5837ffb3a2b036a8dae820f000720272b2dc70a
Luke-kb <keananbrown@gmail.com>
2020-04-26 15:22:15 +1000

    cleaned up code

--- projects/terminal-app/src/app.rb
+++ projects/terminal-app/src/app.rb
@@ -2,9 +2,11 @@ require 'tty-box'
 require 'tty-cursor'
 require 'tty-progressbar'
 require 'tty-prompt'
+require 'tty-screen'
 require 'tty-spinner'
 require 'terminal-table'
 require 'colorize'
+require 'justify'
 require 'artii'
 require 'lolcat'
 require_relative './QuestionBank.rb'
@@ -15,10 +17,13 @@ require_relative './methods.rb'
 @username = ""
 @question_index = 0
 @question_amount = 5
+#score table array
 @rows = []
-
 #set @cursor variable
 @cursor = TTY::Cursor
+
+ARGV.each { |option| process_argv(option) }
+
 #create QuestionBank instance
 @questions = QuestionBank.new
 
@@ -33,24 +38,17 @@ print @cursor.clear_lines(2, :up)
 simple_spin(1, "pulse_3")
 print @cursor.clear_lines(2, :up)
 
-check_username
-
-print @cursor.clear_lines(4, :up) 
-any_key("READY? hit spacebar for the first question..")
-print @cursor.clear_lines(3, :up)
-print @cursor.down(1)
+username
 
 #run quiz questions
 quiz_loop
 
 print @cursor.clear_lines(2, :up)
 
-clear
-#show score
-print_score
-puts "\n"
-score_table
-any_key("press spacebar")
-clear_line_slowly(30)
+print_score_and_table
+
+any_key("\n\npress any key".colorize(:blue))
+clear_line_slowly(28)
 play_again
+
 }
