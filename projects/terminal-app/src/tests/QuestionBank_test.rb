e49b237dc6264db5dfb14b8a88272c59096b9078
Luke-kb <keananbrown@gmail.com>
2020-04-27 14:51:50 +1000

    created tests folder

---
+++ projects/terminal-app/src/tests/QuestionBank_test.rb
@@ -0,0 +1,16 @@
+require 'test/unit'
+
+require_relative '../QuestionBank.rb'
+
+
+class QuestionBankTest < Test::Unit::TestCase
+
+  def test_trivia_databse_api
+
+    test_questions = QuestionBank.new
+
+
+  end
+
+end
+
